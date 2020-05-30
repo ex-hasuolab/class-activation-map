bffbbbafd6a2ce0dc04b3f9c850e66151a6c2abc
TakehikoEsaka <take.pbfzbf302@gmail.com>
2020-05-30 12:55:02 +0900

    add files

---
+++ efficientnet.py
@@ -0,0 +1,30 @@
+# モジュールの構造：uitils.modules.pyに記述
+from efficientnet import EfficientNetB0
+from keras.layers import Input, Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from keras.models import Model
+
+class EfficientNet(object):
+    def __init__(self, cut_size, channel, category_count):
+        self.cut_size = cut_size
+        self.channel = channel
+        self.category_count = category_count
+        
+    def bottleneck(self, x):
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dropout(rate=0.25)(x)
+        x = Dense(self.category_count, activation='sigmoid')(x)
+        return x
+    
+    def getTrainModel(self):
+        input_shape = (self.cut_size["height"], self.cut_size["width"], self.channel)
+        print("input_shape : {}".format(input_shape))
+        
+        # EfficientNetのオプションについては随時調査（やりたいのは１から学習）
+        effnet_instance = EfficientNetB0(input_shape = input_shape,  weights='imagenet', include_top=False)
+        x = effnet_instance.output
+        x = self.bottleneck(x)
+
+        model = Model(inputs=effnet_instance.input, outputs=x)
+        return model
---
+++ solver.py
@@ -0,0 +1,32 @@
+%tensorflow_version 1.x
+from keras.optimizers import RMSprop
+from keras.callbacks import LearningRateScheduler, CSVLogger, ModelCheckpoint, History
+
+class Solver(object):
+    def __init__(self, dataloder_instance, model_instance, batch_size, n_epochs):
+        self.dataloder_instance = dataloder_instance
+        self.model_instance = model_instance
+        self.batch_size = batch_size
+        self.n_epochs = n_epochs
+    
+    def get_model(self):
+        self.model = self.model_instance.getTrainModel()
+        # mnist : 'categorical_crossentropy'
+        # solar : "binary_crossentropy"
+        # optimizer = daget_adam_for_fine_tuning(lr=1e-3, decay=1e-5, multiplier=0.01, model=model)
+        self.model.compile(optimizer=RMSprop(), loss='binary_crossentropy', metrics=['categorical_accuracy'])
+        return self.model
+        
+    def train(self, model):
+        # callbackを定義する場合はここに記述
+        print("x_train shape {}".format(self.dataloder_instance.x_train.shape))
+        print("y_train shape {}".format(self.dataloder_instance.y_train.shape))
+
+        history = Histories(self.epochs, self.initial_lr, self.drop, self.epochs_drop, self.accuracy, self.loss, self.learning_rate)
+        lr_schedule = LearningRateScheduler(lrs)
+        model_checkpoint = ModelCheckpoint("final_weights_step1.hdf5", monitor = 'val_loss', verbose = 1,
+                                            save_best_only = True, save_weights_only = True, period = 1)
+
+        model.fit(self.dataloder_instance.x_train, self.dataloder_instance.y_train, batch_size=self.batch_size, \
+                  epochs=self.n_epochs, verbose=1, validation_data=(self.dataloder_instance.x_valid, self.dataloder_instance.y_valid), \
+                  callbacks = [lr_schedule, model_checkpoint])
