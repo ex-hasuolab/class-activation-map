3d9dd1832b835b8d1f9bef9952cd3b8fe663ba48
TakehikoEsaka <take.pbfzbf302@gmail.com>
2020-06-13 13:25:39 +0900

    solverにtrain_fitgeneraotr関数を追加

--- solver.py
+++ solver.py
@@ -2,10 +2,9 @@ from keras.optimizers import RMSprop
 from keras.callbacks import LearningRateScheduler, CSVLogger, ModelCheckpoint, History, TensorBoard
 
 class Solver(object):
-    def __init__(self, dataloader_instance, model_instance, batch_size, n_epochs):
+    def __init__(self, dataloader_instance, model_instance, n_epochs):
         self.dataloader_instance = dataloader_instance
         self.model_instance = model_instance
-        self.batch_size = batch_size
         self.n_epochs = n_epochs
     
     def get_model(self):
@@ -16,7 +15,8 @@ class Solver(object):
         self.model.compile(optimizer=RMSprop(), loss='binary_crossentropy', metrics=['categorical_accuracy'])
         return self.model
         
-    def train(self, model):
+    def train(self, model, batch_size):
+        self.batch_size = batch_size
         # callbackを定義する場合はここに記述
         print("x_train shape {}".format(self.dataloader_instance.x_train.shape))
         print("y_train shape {}".format(self.dataloader_instance.y_train.shape))
@@ -45,3 +45,34 @@ class Solver(object):
                   epochs=self.n_epochs, verbose=1, 
                   validation_data=(self.dataloader_instance.x_valid, self.dataloader_instance.y_valid),
                   callbacks = callbacks)
+        
+    def train_generator(self, model):
+        # callbackを定義する場合はここに記述
+        print("x_train shape {}".format(self.dataloader_instance.x_train.shape))
+        print("y_train shape {}".format(self.dataloader_instance.y_train.shape))
+
+        """
+        # 後で監視が必要ならHistory設定
+        # history = Histories(self.epochs, self.initial_lr, self.drop, self.epochs_drop, self.accuracy, self.loss, self.learning_rate)
+
+        # 学習率をいじる必要があればLearningRateScheduler(lrs)でコントロール
+        # lr_schedule = LearningRateScheduler(lrs)
+        """
+
+        # Modelcheckpoint(filepath, monitor, verbose, save_best_only : 最良モデルをもつ, save_wights_only : Falseなら全体が保存される，period : チェックポイント間のエポック)
+        model_checkpoint = ModelCheckpoint("final_weights_step.hdf5", monitor = 'val_loss', verbose = 1,
+                                  save_best_only = True, save_weights_only = True, period = 1)
+
+        # Tensorboardによる可視化
+        log_dir = "./log/"
+        tensorboard = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+        callbacks = []
+        callbacks.append(model_checkpoint)
+        callbacks.append(tensorboard)
+
+        model.fit_generator(self.dataloader_instance.train_generator, \
+                  epochs=self.n_epochs, verbose=1, \
+                  validation_data=self.dataloader_instance.validation_generator, callbacks = callbacks)
+        
+        # steps_per_epoch = 
