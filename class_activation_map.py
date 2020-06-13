e94a1bd79c9766b78964bb851cb8833558f1cf3f
OsugiTakuya <t.4.103.tkynt@gmail.com>
2020-06-13 15:58:34 +0900

    class_activation_map.pyで GlobalAveragePooling層と全結合層を指定できるよう変更

--- class_activation_map.py
+++ class_activation_map.py
@@ -2,7 +2,7 @@ from keras import Model
 
 
 class ClassActivationMap:
-    def __init__(self, model):
+    def __init__(self, model, layer_GAP=-2, layer_dense=-1):
         """
         kerasのmodelから class activation map を計算する
 
@@ -12,11 +12,24 @@ class ClassActivationMap:
             kerasの学習済みモデル
             ・model.layers[-4] が特徴マップを出力するConv2D層（GlobalAveragePooling2Dの前）
             ・model.layers[-1] が全結合層
+        layer_GAP : int or str
+            GlobalAveragePooling2D層のインデックス、またはレイヤー名
+        layer_dense : int or str
+            全結合層のインデックス、またはレイヤー名
         """
-        # 中間層を出力とするモデルを作成する
-        self.cam_model = Model(inputs=model.input, outputs=model.layers[-4].output)
+        # モデルに含まれるレイヤー名のリストを取得
+        layers_name = [l.name for l in model.layers]
+
+        # レイヤー名で指定された場合はインデックスに変換
+        if type(layer_GAP) is str:
+            layer_GAP = layers_name.index(layer_GAP)
+        if type(layer_dense) is str:
+            layer_dense = layers_name.index(layer_dense)
+
+        # GlobalAveragePooling2D層の直前を出力とするモデルを作成する
+        self.cam_model = Model(inputs=model.input, outputs=model.layers[layer_GAP-1].output)
         # 全結合層の重み、バイアスを作成する
-        self.w, self.b = model.layers[-1].get_weights()
+        self.w, self.b = model.layers[layer_dense].get_weights()
     
 
     def get_class_activation_map(self, images):
